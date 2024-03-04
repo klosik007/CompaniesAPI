f3c8f1f672731cba1064429c44678007f7ba034a
Przemysław Kłos <pklos1992@gmail.com>
2024-03-04 09:40:13 +0100

    CompanyFixtures companies references for EmployeeFixtures

--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -8,6 +8,9 @@ use Doctrine\Persistence\ObjectManager;
 
 class CompanyFixtures extends Fixture
 {
+    public const COMPANY1_REFERENCE = 'company1';
+    public const COMPANY2_REFERENCE = 'company2';
+
     public function load(ObjectManager $manager): void
     {
         $company1 = new Company();
@@ -15,6 +18,7 @@ class CompanyFixtures extends Fixture
         $company1->setAddress("Kolejowa 54");
         $company1->setPostal("00-999");
         $company1->setNIP("0258741369");
+        $this->addReference(self::COMPANY1_REFERENCE, $company1);
         $manager->persist($company1);
 
         $company2 = new Company();
@@ -22,6 +26,7 @@ class CompanyFixtures extends Fixture
         $company2->setAddress("Wartowna 54");
         $company2->setPostal("00-777");
         $company2->setNIP("1236547890");
+        $this->addReference(self::COMPANY2_REFERENCE, $company2);
         $manager->persist($company2);
 
         $manager->flush();
